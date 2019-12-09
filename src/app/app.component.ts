ab3121f24ac81f2303d1449f8eccbaa94cbc5aed
Lora Tzoneva <lora.tzoneva@cantanea.com>
2019-12-09 15:06:29 +0200

    hamburger menu + fixed margins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lsp';
+  navbarOpen = false;
+
+	toggleNavbar() {
+		this.navbarOpen = !this.navbarOpen;
+	}
 }
+
