1d281ec3dafad3228dc83855f1be7b85d9cc9c1d
Lora Tzoneva <lora.tzoneva@cantanea.com>
2019-12-13 11:50:34 +0200

    header + toggle menu material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,31 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+	MainNavComponent
   ],
   imports: [
-    BrowserModule,
+	BrowserModule,
+	BrowserAnimationsModule,
 	AppRoutingModule,
-	NgbModule
+	NgbModule,
+	LayoutModule,
+	MatToolbarModule,
+	MatButtonModule,
+	MatSidenavModule,
+	MatIconModule,
+	MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
