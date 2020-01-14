8fa769f00cba75e77f34f1dcbb07fc3ff17faa2c
Lora Tzoneva <lora.tzoneva@cantanea.com>
2020-01-14 18:23:51 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
@@ -11,7 +10,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
 import { FooterComponent } from './footer/footer.component';
 
 
@@ -25,7 +23,6 @@ import { FooterComponent } from './footer/footer.component';
 	BrowserModule,
 	BrowserAnimationsModule,
 	AppRoutingModule,
-	NgbModule,
 	LayoutModule,
 	MatToolbarModule,
 	MatButtonModule,
