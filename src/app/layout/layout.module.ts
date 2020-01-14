c9828ae6fa4a34e5f642aeebf514fee195681fb4
Lora Tzoneva <lora.tzoneva@cantanea.com>
2020-01-14 18:59:32 +0200

    added sidenav component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+	declarations: [SidenavComponent],
+	imports: [
+		CommonModule,
+		MatSidenavModule
+	]
 })
 export class LayoutModule { }
