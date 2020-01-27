1447e94dedb94053eab463d57876fe7ce83797ec
Lora Tzoneva <lora.tzoneva@cantanea.com>
2020-01-27 18:04:33 +0200

    search form frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 
 @NgModule({
@@ -27,6 +32,10 @@ import { FooterComponent } from './footer/footer.component';
 	MatSidenavModule,
 	MatIconModule,
 	MatListModule,
+	MatInputModule,
+	MatFormFieldModule,
+	MatSelectModule,
+	MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
